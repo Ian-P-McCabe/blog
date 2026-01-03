b684161099bde66f8f98923c1c20a6813600d39b
Ian McCabe <imccabe@deloitte.com>
2026-01-03 12:36:45 -0500

    Module imports are working

--- src/dodgeball_colors_3/color_sorting.py
+++ src/dodgeball_colors_3/color_sorting.py
@@ -1,4 +1,5 @@
 from colorsys import rgb_to_hsv
+from colormath import color_diff
 
 def hex_to_hsv(hex_color):
     """Convert hex color to HSV tuple (hue, saturation, value)"""
@@ -13,3 +14,15 @@ def adjust_hue_for_rainbow(hex_color):
     # Rotate hue by -0.1 so red (which is ~0) starts first
     h = (h + 0.15) % 1.0
     return (h, s, v)   
+
+
+def build_distance_matrix(lab_colors):
+  distance_matrix = {}
+  for i, color1 in enumerate(lab_colors):
+      for j, color2 in enumerate(lab_colors):
+          if i != j:
+            distance_matrix[(i, j)] = round( #Want integer values
+              color_diff.delta_e_cie2000(color1=color1, color2=color2)
+              )
+  
+  return distance_matrix
