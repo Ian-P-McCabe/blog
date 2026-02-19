cdf6eb754efe24d348035f2add36262d98237b3f
Ian McCabe <ianpmccabe@gmail.com>
2026-02-19 10:23:07 -0500

    Module imports are working

---
+++ src/dodgeball_colors_3/__init__.py
@@ -0,0 +1,4 @@
+from .display_functions import display_palette, plot_distance_heatmap, display_color_swatches
+from .color_sorting import adjust_hue_for_rainbow, build_distance_matrix
+from .color_model import ColorModel
+
